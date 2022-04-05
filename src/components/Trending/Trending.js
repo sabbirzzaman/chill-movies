20ca7ee10b5947ec2d8d18f8675b8c3ff378a199
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-06 01:31:59 +0600

    home page update

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -3,21 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import SingleContent from '../SingleContent/SingleContent';
 
 const Trending = ({ content, title, directory }) => {
-    const trending = content.slice(0, 4);
+    const trending = content.slice(0, 6);
     const navigate = useNavigate();
 
     return (
-        <div className="max-w-[325px] mx-auto sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px]">
-            <div className="flex justify-between items-center">
-                <h2 className="text-2xl font-extrabold">{title}</h2>
+        <div className="max-w-[325px] mx-auto sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] text-center">
+            <div className="flex justify-center items-center sm:justify-between sm:mb-8">
+                <h2 className="text-2xl font-extrabold mb-8 sm:mb-0">{title}</h2>
                 <button
                     onClick={() => navigate(directory)}
-                    className="py px-6 md:px-10 text-center bg-white text-black font-bold text-base cursor-pointer transition-all delay-75 hover:bg-red-500 hover:text-white"
+                    className="hidden sm:inline py px-6 md:px-10 text-center bg-white text-black font-bold cursor-pointer transition-all delay-75 hover:bg-red-500 hover:text-white"
                 >
                     View All
                 </button>
             </div>
-            <div className="py-8 grid sm:grid-cols-2 md:grid-cols-3 md:gap-2 lg:grid lg:grid-cols-4 lg:gap-8">
+            <div className="grid sm:grid-cols-2 sm:mb-10 md:grid-cols-3 md:gap-2 lg:grid lg:grid-cols-6 lg:gap-8">
                 {trending.map((content) => (
                     <SingleContent
                         key={content.id}
@@ -25,6 +25,12 @@ const Trending = ({ content, title, directory }) => {
                     ></SingleContent>
                 ))}
             </div>
+            <button
+                onClick={() => navigate(directory)}
+                className="mb-10 py px-6 bg-white text-black font-bold cursor-pointer transition-all delay-75 hover:bg-red-500 hover:text-white sm:hidden"
+            >
+                View All
+            </button>
         </div>
     );
 };
