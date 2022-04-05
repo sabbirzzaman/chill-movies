29801fe73e86e0ea4942e38fb628da666f45ceaa
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-05 18:07:47 +0600

    tv series page completed

--- src/components/TvSeries/TvSeries.js
+++ src/components/TvSeries/TvSeries.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useContent from '../../hooks/useContent';
+import SingleContent from '../SingleContent/SingleContent';
 
 const TvSeries = () => {
+    const urlLink = 'https://api.themoviedb.org/3/trending/tv/day?api_key=';
+
+    const [tvSeries] = useContent(urlLink);
+
+    console.log(tvSeries)
+
     return (
-        <div>
-            <h2>This is tv series!!</h2>
+        <div className='py-8 md:grid md:grid-cols-2 md:gap-2 lg:grid lg:grid-cols-3 lg:gap-8'>
+            {tvSeries.map((tv) => (
+                <SingleContent key={tv.id} content={tv}></SingleContent>
+            ))}
         </div>
     );
 };
